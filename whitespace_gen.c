32fda11e7e68051fa26fb8c26f581370f6ab74c1
sebbeobe <sebbeobe@users.noreply.github.com>
2017-04-07 16:07:06 +0200

    Update whitespace_gen.c
    Shorter code.

--- whitespace_gen.c
+++ whitespace_gen.c
@@ -1,43 +1,30 @@
 #include<stdio.h>    	    		
-#include<stdlib.h>	
-#include<string.h>      		 		  
-	
-void padbin(char *bin_num)    		  	 	
+#include<string.h>	
+int main()     		 		  
 {	
-int len=9-strlen(bin_num);char new[len];    			 		 
+FILE *fp; char message[100]; char whitespace[1600]=""; 		  	 	
 	
-for(int i=0;i<len-1;i++) new[i]='0';    		  	 	
-strcat(new,bin_num);	
-strcpy(bin_num,new);      			  	 
-}	
-       	 		  
-	
-int main()      	     
-{	
-FILE *fp;char message[100]; char whitespace[1600]=""; char binary[9];		   		
-	
-printf("Write your message here (max 100 characters):\n");		 		  
-gets(message);	
+printf("Write your message here (max 99 characters):\n");			 		 
+fgets(message,100,stdin);	
 for(int i=0;message[i]!=0;i++)     		  	 	
 {	
-strcat(whitespace,"   ");   			 		 
-itoa(message[i],binary,2);	
-padbin(binary);      		  	 	
+strcat(whitespace,"   ");   			  	 
 	
-for(int j=0;binary[j]!=0;++j)     			  	 
+for(int j=7;j>-1;--j)      	     
 {	
-if(binary[j]=='1')strcat(whitespace,"\t"); else strcat(whitespace," ");    	     
+      		  			
+if(message[i]&(1<<j))strcat(whitespace,"\t");	
+else strcat(whitespace," ");    		 	  	
 }	
-strcat(whitespace,"\n\t\n  ");    		  			
+strcat(whitespace,"\n\t\n  ");    			  	 
 	
-}      		 	  	
-strcat(whitespace,"\n\n\n");	
-      			  	 
+}      		 		  
+	
+strcat(whitespace,"\n\n\n");       	    	
 fp=fopen("whitespace_code_file","w");	
-fputs(whitespace,fp);      		 		  
+fputs(whitespace,fp);         	 	 
 fclose(fp);	
-return 0;      	    	
-	
-}  
+return 0; 
 
 
+}
